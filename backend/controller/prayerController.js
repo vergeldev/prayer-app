041abb7e881dd48f59cce393905e7cb097f75cb9
vergeldev <vergeldev@gmail.com>
2023-04-16 22:42:08 +0800

    added express-async-handler

--- backend/controller/prayerController.js
+++ backend/controller/prayerController.js
@@ -1,21 +1,23 @@
-const getPrayer = async (req, res) => {
-  res.json({ message: "getting prayers" });
-};
+const asyncHandler = require("express-async-handler");
 
-const postPrayer = async (req, res) => {
+const getPrayer = asyncHandler(async (req, res) => {
+  res.json({ message: "getting prayers" });
+});
+
+const postPrayer = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error("Please submit a text");
   }
   res.json({ message: "creating a prayer" });
-};
+});
 
-const putPrayer = async (req, res) => {
+const putPrayer = asyncHandler(async (req, res) => {
   res.json({ message: `updating prayer for ${req.params.id}` });
-};
+});
 
-const deletePrayer = async (req, res) => {
+const deletePrayer = asyncHandler(async (req, res) => {
   res.json({ message: `deleting prayer for ${req.params.id}` });
-};
+});
 
 module.exports = { getPrayer, postPrayer, putPrayer, deletePrayer };
