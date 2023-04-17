16282ba8bcc6bb4d94a5c98a711bbf5097e4a0f7
vergeldev <vergeldev@gmail.com>
2023-04-17 20:12:04 +0800

    Auth added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
-const { errorHandler } = require("../backend/errorMiddleware/errorHandler");
+const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("../backend/config/db");
 const colors = require("colors");
 const port = process.env.PORT || 5000;
@@ -12,7 +12,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/api/prayers", require("./routes/routePrayers"));
+app.use("/api/prayers", require("./routes/prayerRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 
 app.use(errorHandler);
 
