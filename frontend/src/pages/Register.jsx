1961ad6778f87d49ea2e3085d0db31c757270459
vergeldev <vergeldev@gmail.com>
2023-04-21 13:15:52 +0800

    minor changes

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,4 +1,7 @@
 import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 function Register() {
   const [formData, setFormData] = useState({
